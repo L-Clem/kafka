345a12132180891718224eae228fe957ffa1ed19
Clément <japanclems287@gmail.com>
2024-01-22 14:38:41 +0100

    truc

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     #rank = currency_rank_poller(currency)
     for x in range(50):
         rate = currency_rate_poller(currency)
-        send("currency_rate_evolution", currency, "{key: \"" + currency + "\", val: " + rate + "}")
+        send("currency_rate_evolution", currency, "{\"key\": \"" + currency + "\", \"val\": " + rate + "}")
     #consume(["currency_mean_volume_month"])
 
 if __name__ == "__main__":
--- transform.py
+++ transform.py
@@ -4,9 +4,28 @@ from pyflink.datastream.formats.json import JsonRowDeserializationSchema
 from pyflink.datastream.window import TumblingProcessingTimeWindows
 from pyflink.common import WatermarkStrategy, Time
 from pyflink.common.typeinfo import Types
+from pyflink.datastream.functions import AggregateFunction
+
+class CalculateDifferenceAggregate(AggregateFunction):
+    def create_accumulator(self):
+        return (None, None)
+
+    def add(self, value, accumulator):
+        return (accumulator[1], value[2])
+
+    def get_result(self, accumulator):
+        if accumulator[0] is not None:
+            return accumulator[1] - accumulator[0]
+        else:
+            return 0.0  
+
+    def merge(self, a, b):
+        return (a[0], b[1])
+
 
 env = StreamExecutionEnvironment.get_execution_environment()
-env.add_jars('file:///flink-sql-connector-kafka-1.17.2.jar')
+env.add_jars("file:///tmp/flink-sql-connector-kafka-1.17.2.jar")
+print(env)
 
 source = KafkaSource.builder() \
     .set_bootstrap_servers("localhost:9092") \
@@ -22,6 +41,10 @@ source = KafkaSource.builder() \
 
 stream = env.from_source(source, WatermarkStrategy.no_watermarks(), "currency_rate_evolution")
 
-stream.print()
+stream.map(lambda ligne : (ligne[0], f"{ligne[1]}", ligne[1]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()])) \
+    .key_by(lambda ligne : ligne[0]) \
+    .window(TumblingProcessingTimeWindows.of(Time.seconds(10))) \
+    .aggregate(CalculateDifferenceAggregate(), output_type=Types.FLOAT()) \
+    .print()
 
 env.execute()
